26441f9b72d8193cb6e0de899439fb38d3697217
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-03 23:37:16 +0200

    Ajout des nouvelles pages dans le router

--- AreneUnifiedChampions-ProjetS2/src/router/index.js
+++ AreneUnifiedChampions-ProjetS2/src/router/index.js
@@ -2,13 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
 import ContactView from '../views/ContactView.vue';
 import MentionslegalesView from '../views/MentionslegalesView.vue';
-
+import HistoireView from '../views/HistoireView.vue';
+import ReglementView from '../views/ReglementView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Accueil', component: AccueilView },
     { path: '/contact', name: 'ContactView', component: ContactView },
     { path: '/mentions', name: 'MentionslegalesView', component: MentionslegalesView },
+    { path: '/histoire', name: 'HistoireView', component: HistoireView },
+    { path: '/reglement', name: 'ReglementView', component: ReglementView },
   ]
 })
 
