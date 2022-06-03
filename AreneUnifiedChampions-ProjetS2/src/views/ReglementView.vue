ceaa0bafc2d6d8e0df59160d484e333d59f3f0b9
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-04 00:08:41 +0200

    import header footer page reglement

--- AreneUnifiedChampions-ProjetS2/src/views/ReglementView.vue
+++ AreneUnifiedChampions-ProjetS2/src/views/ReglementView.vue
@@ -1,3 +1,13 @@
+<script>
+import header1 from "../components/Header.vue"
+import footer1 from "../components/Footer.vue"
+export default {
+    name:"App",
+    components: { header1, footer1 },
+}
+</script>
 <template>
-    
+<header1 />
+
+<footer1 />
 </template>
