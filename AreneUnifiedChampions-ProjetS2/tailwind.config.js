179f4dd661e557c1efdadb222106d4c62c83aad5
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-03 23:36:41 +0200

    import police tailwind config

--- AreneUnifiedChampions-ProjetS2/tailwind.config.js
+++ AreneUnifiedChampions-ProjetS2/tailwind.config.js
@@ -9,33 +9,34 @@ module.exports = {
          "red-800": "#991b1b",
          "Festival-info": "#f9fafb"
         },
-        "fontSize": {
-         "sm": "0.9375rem",
-         "base": "2rem"
-        },
-        "fontFamily": {
-         "algerian": "Algerian",
-         "inter": "Inter"
-        },
-        "borderRadius": {
-         "none": "0",
-         "xs": "0.18370576202869415rem",
-         "sm": "0.25rem",
-         "default": "0.2724449634552002rem",
-         "lg": "0.3125rem",
-         "xl": "0.3508129417896271rem",
-         "2xl": "0.36270490288734436rem",
-         "3xl": "0.375rem",
-         "4xl": "0.5080958604812622rem",
-         "5xl": "0.9375rem",
-         "6xl": "1rem",
-         "7xl": "1.5625rem",
-         "8xl": "2.1875rem",
-         "9xl": "2.8271484375rem",
-         "10xl": "5.0888671875rem",
-         "full": "9999px"
-        }
-    },
+          "fontSize": {
+            "sm": "0.9375rem",
+            "base": "2rem"
+          },
+          "fontFamily": {
+           "circular-std": "Circular Std",
+           "rajdhani": "Rajdhani",
+           "museomoderno": "MuseoModerno"
+          },
+          "borderRadius": {
+           "none": "0",
+           "xs": "0.0625rem",
+           "sm": "0.1875rem",
+           "default": "0.25rem",
+           "lg": "0.3125rem",
+           "xl": "0.625rem",
+           "2xl": "0.6875rem",
+           "3xl": "0.8125rem",
+           "4xl": "0.9375rem",
+           "5xl": "1rem",
+           "6xl": "1.1875rem",
+           "7xl": "1.25rem",
+           "8xl": "1.375rem",
+           "9xl": "1.5625rem",
+           "10xl": "1.8125rem",
+           "11xl": "2.1875rem"
+          }
+         }
   },
   plugins: [],
 }
