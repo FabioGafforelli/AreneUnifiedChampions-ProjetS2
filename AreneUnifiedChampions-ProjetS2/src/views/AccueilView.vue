41d5ea79eef6f3fd773b9610e5ea93ca7d541744
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-05 12:38:17 +0200

    Page d'accueil fini et responsive

--- AreneUnifiedChampions-ProjetS2/src/views/AccueilView.vue
+++ AreneUnifiedChampions-ProjetS2/src/views/AccueilView.vue
@@ -10,8 +10,8 @@ export default {
 
 <template class="bg-indigo-25">
     <header1 />
-    <div class="bg-indigo-25">
-                <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  ">
+    <div class="bg-indigo-25 ">
+    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  ">
 <div>
     <img src="/public/images/boutonjoueur.png" alt="Scroller jusqu'à la page description joueur en cliquant sur cette image">
 </div>
@@ -22,24 +22,95 @@ export default {
     <img src="/public/images/boutontournoi.png" alt="Scroller jusqu'à la page tournoi en cliquant sur cette image">
 </div>
                 </div>
-                
-<section class="flex grid-cols-3">
+            
+<div class="flex grid-cols-3">
         <SectionJoueur />
-       <h1 class="text-black text-5xl px-4 font-rajdhani">Vous êtes un</h1><h1 class="text-orange-400 text-5xl font-rajdhani"> Joueur ? </h1>
-
-</section>
-       <p class="text-black text-3xl font-rajdhani font-bold bottom-5">Vous êtes un joueur, mais vous n’avez pas d’équipe pour la Grosse ligue ?</p>
-       <p class="text-black text-2xl font-rajdhani font-light">Pas de soucis, vous pouvez postuler en créant votre profil pour rejoindre une équipe.</p>
+     <div class=""> <h1 class="text-black text-5xl px-4 font-rajdhani">Vous êtes un</h1><h1 class="text-5xl text-transparent bg-clip-text bg-gradient-to-br from-orange-600 to-red-800"> Joueur ? </h1>
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-bold bottom-5">Vous êtes un joueur, mais vous n’avez pas d’équipe pour la Grosse ligue ?</p>
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-light">Pas de soucis, vous pouvez postuler en créant votre profil pour rejoindre une équipe.</p>
        <br>
-       <p class="text-black text-2xl font-rajdhani font-light">Dans ce profil vous pouvez renseignez toute votre expérience sur le jeu “League of Legend”
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-light">Dans ce profil vous pouvez renseignez toute votre expérience sur le jeu “League of Legend”
 (votre rang, votre rôle...).</p>
+
 <br>
-       <p class="text-black text-2xl font-rajdhani font-normal">Vous pouvez vous rendre dans la base de donées mise à votre dispotition, Afin de trouver l’équipe qui vous correspond.</p>
+       <p class="text-black text-2xl font-rajdhani font-normal">Vous pouvez vous rendre dans la base de donées mise à votre dispotition, Afin de trouver l’équipe qui vous correspond.</p></div>
+</div>
 <div class="grid grid-cols-3">
-    <img src="/public/images/CardsJoueurs.png" alt="">
+    <div class="bg-gray-200 rounded-xl ml-2"> 
+        <div><h2 class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-orange-300 to-red-800">3 étapes</h2></div>
+        <br>
+        <div class=""><p class=" text-2xl font-museomoderno font-bold text-orange-600">1.</p> <p class="font-rajdhani font-bold text-black text-2xl">Créer</p><p class="ml-3 text-2xl">votre profil</p></div>
+        <br>
+        <div class=""><p class=" text-2xl font-museomoderno font-bold text-orange-600">2.</p> <p class="font-rajdhani font-bold text-black text-2xl">Rechercher</p><p class="ml-3 text-2xl">une équipe dans notre 
+base de données en fonction de vos envies</p></div>
+        <br>
+        <div class=""><p class=" text-2xl font-museomoderno font-bold text-orange-600">3.</p> <p class="font-rajdhani font-bold text-black text-2xl"> Intégrer</p><p class="ml-3 text-2xl">votre nouvelle équipe et GAGNEZ !</p></div>
+        </div>
     <button><img src="/public/images/BoutonProfil.png" alt=""></button>
     <button><img src="/public/images/Boutonchercherequipe.png" alt=""></button>
 </div>
+<br>
+<div class="flex grid-cols-3">
+        <img class="w-12 h-12" src="/public/images/SectionEquipe.png" alt="">
+     <div class=""> <h1 class="text-black text-5xl px-4 font-rajdhani">Vous êtes une</h1><h1 class="text-pink-400 text-5xl font-rajdhani"> Equipe ? </h1>
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-bold bottom-5">Vous souhaitez inscrire votre équipe pour la Grosse ligue ? </p>
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-light">Vous pouvez vous inscrire  en créant votre équipe pour participer au tournoi de qualification.</p>
+       <br>
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-light">Vous pouvez naviguez dans notre base de données
+pour voir les profils des joueurs souhaitants rejoindre une équipe. </p>
+
+<br>
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani font-normal">Vous pouvez créer votre équipe afin de gagner en visibilité.</p></div>
+</div>
+<div class="grid grid-cols-3">
+    <div class="bg-gray-200 rounded-xl ml-2"> 
+        <div><h2 class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-300 to-purple-800">3 étapes</h2></div>
+        <br>
+        <div class=""><p class=" text-2xl font-museomoderno font-bold text-blue-600">1.</p> <p class="font-rajdhani font-bold text-black text-2xl">Créer</p><p class="ml-3 text-2xl">votre équipe.</p></div>
+        <br>
+        <div class=""><p class=" text-2xl font-museomoderno font-bold text-purple-600">2.</p> <p class="font-rajdhani font-bold text-black text-2xl">Rechercher</p><p class="ml-3 text-2xl">des joueurs dans notre
+ base de données en fonction de vos envies</p></div>
+        <br>
+        <div class=""><p class=" text-2xl font-museomoderno font-bold text-pink-600">3.</p> <p class="font-rajdhani font-bold text-black text-2xl"> Recrutez</p><p class="ml-3 text-2xl">des nouveaux joueurs et GAGNEZ !</p></div>
+        </div>
+    <button><img src="/public/images/BoutonEquipe.png" alt="Bouton permettant d'accèder à la page création d'une équipe"></button>
+    <button><img src="/public/images/Boutonchercherjoueur.png" alt="Bouton permettant d'accèder à la liste de tous les joueurs inscrits"></button>
+</div>
+<br>
+<div class="flex grid-cols-3">
+        <img class="w-12 h-12" src="/public/images/Sectiontournois.png" alt="">
+     <div class=""> <h1 class="text-black text-5xl px-4 font-rajdhani">Annonces</h1><h1 class="text-green-600 text-5xl font-rajdhani"> Tournois </h1>
+       <p class="text-black text-2xl font-rajdhani font-light">Retrouvez le tournoi esport League of Legend afin de départager les 
+meilleures équipes universitaires de  Bourgogne Franche-Comté qui 
+participera à la grosse ligue.</p>
+       <br>
+<br>
+       <p class="text-black text-2xl font-rajdhani font-normal">Vous retouverez ici l’agenda des match du tournoi ainsi que les rediffusion 
+des matchs.</p></div>
+</div>
+<div class="grid grid-cols-1">
+    <br>
+<div><h1 class="font-bold font-rajdhani flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl">Calendrier des matchs</h1></div>
+</div>
+<div class="grid grid-cols-2">
+    <div class="flex mr-auto my-auto"><img class="w-24 h-24" src="/public/images/CalendrierMatch.png" alt="">
+    <h2 class="flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani">Matchs du jour</h2>
+    </div>
+    <div>
+       <div class="flex"><img class="w-24 h-24" src="/public/images/Calendrier.png" alt="">
+            <h2 class="flex pl-5 pt-5 pb-10 sm:text-2xl lg:text-2xl xl:text-3xl font-rajdhani">Matchs à venir</h2>
+    </div>
+    </div>
+</div>
+<br>
+<div class="grid grid-cols-2 bg-gradient-to-bl from-lime-300 to-blue-700">
+<div><img src="/public/images/Card_matchs1.png" alt="Affiche d'un match à venir du tournoi de qualification le 10 juin 2022 à 19h"></div>
+<div>
+    <img src="/public/images/Card_matchs2.png" alt="Affiche d'un match à venir du tournoi de qualification le 17 juin 2022 à 18 heures 30">
+    <img src="/public/images/Cars_matchs3.png" alt="Affiche d'un match à venir du tournoi de qualification le 24 juin 2022 à 17 heures 30">
+</div>
+</div>
+
 
 </div>
 <footer1 />
