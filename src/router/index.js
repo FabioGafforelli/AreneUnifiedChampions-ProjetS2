7ab47e8899eff878d06c9ebd14c0ee71406f3bc2
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-05 18:25:03 +0200

    Déplacement des fichiers suite à une mauvaise manip en début de dossier

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import ContactView from '../views/ContactView.vue';
 import MentionslegalesView from '../views/MentionslegalesView.vue';
 import HistoireView from '../views/HistoireView.vue';
 import ReglementView from '../views/ReglementView.vue';
+import InscriptionView from '../views/InscriptionView.vue';
+import JeuxView from '../views/JeuxView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,6 +14,8 @@ const router = createRouter({
     { path: '/mentions', name: 'MentionslegalesView', component: MentionslegalesView },
     { path: '/histoire', name: 'HistoireView', component: HistoireView },
     { path: '/reglement', name: 'ReglementView', component: ReglementView },
+    { path: '/inscription', name: 'InscriptionView', component: InscriptionView },
+    { path: '/jeux', name: 'JeuxView', component: JeuxView },
   ]
 })
 
