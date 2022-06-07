d4b79be5224dd99a69f62ab3f68aee80d5bed63c
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-08 00:58:48 +0200

    Responsive du texte page d'accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -45,15 +45,15 @@ export default {
         <div><h2 class="ml-4 mt-4 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-red-700">3 étapes</h2></div>
         <br>
         <ul class="list-disc">
-            <span class="ml-4 mt-4 text-2xl font-museomoderno font-bold text-orange-600">1.</span>
-             <p class="font-rajdhani font-bold text-2xl text-gray-600 hover:text-black">Créer</p>
-             <p class="ml-3 text-2xl ">votre profil</p>
+            <span class="ml-4 mt-4 flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-museomoderno font-bold text-orange-600">1.</span>
+             <p class="font-rajdhani font-bold flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl text-gray-600 hover:text-black">Créer</p>
+             <p class="ml-3 flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl ">votre profil</p>
              </ul>
         <br>
-        <div class=""><p class=" text-2xl font-museomoderno font-bold text-orange-600">2.</p> <p class="font-rajdhani font-bold text-black text-2xl">Rechercher</p><p class="ml-3 text-2xl">une équipe dans notre 
+        <div class=""><p class="flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-museomoderno font-bold text-orange-600">2.</p> <p class="font-rajdhani font-bold text-black text-2xl">Rechercher</p><p class="ml-3 text-2xl">une équipe dans notre 
 base de données en fonction de vos envies</p></div>
         <br>
-        <div class=""><p class=" text-2xl font-museomoderno font-bold text-orange-600">3.</p> <p class="font-rajdhani font-bold text-black text-2xl"> Intégrer</p><p class="ml-3 text-2xl">votre nouvelle équipe et GAGNEZ !</p></div>
+        <div class=""><p class="flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-museomoderno font-bold text-orange-600">3.</p> <p class="font-rajdhani font-bold text-black text-2xl"> Intégrer</p><p class="ml-3 text-2xl">votre nouvelle équipe et GAGNEZ !</p></div>
         </div>
 </div>
 
@@ -64,26 +64,26 @@ base de données en fonction de vos envies</p></div>
 
 <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
     <div class="bg-gray-200 rounded-xl ml-2"> 
-        <div><h2 class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-300 to-purple-800">3 étapes</h2></div>
+        <div><h2 class="flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-300 to-purple-800">3 étapes</h2></div>
         <br>
-        <div class=""><p class=" text-2xl font-museomoderno font-bold text-blue-600">1.</p> <p class="font-rajdhani font-bold text-black text-2xl">Créer</p><p class="ml-3 text-2xl">votre équipe.</p></div>
+        <div class=""><p class="flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-museomoderno font-bold text-blue-600">1.</p> <p class="font-rajdhani font-bold text-black text-2xl">Créer</p><p class="ml-3 text-2xl">votre équipe.</p></div>
         <br>
-        <div class=""><p class=" text-2xl font-museomoderno font-bold text-purple-600">2.</p> <p class="font-rajdhani font-bold text-black text-2xl">Rechercher</p><p class="ml-3 text-2xl">des joueurs dans notre
+        <div class=""><p class="flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-museomoderno font-bold text-purple-600">2.</p> <p class="font-rajdhani font-bold text-black text-2xl">Rechercher</p><p class="ml-3 text-2xl">des joueurs dans notre
  base de données en fonction de vos envies</p></div>
         <br>
-        <div class=""><p class=" text-2xl font-museomoderno font-bold text-pink-600">3.</p> <p class="font-rajdhani font-bold text-black text-2xl"> Recrutez</p><p class="ml-3 text-2xl">des nouveaux joueurs et GAGNEZ !</p></div>
+        <div class=""><p class="flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-museomoderno font-bold text-pink-600">3.</p> <p class="font-rajdhani font-bold text-black text-2xl"> Recrutez</p><p class="ml-3 text-2xl">des nouveaux joueurs et GAGNEZ !</p></div>
         </div>
 </div>
 <br>
 <div class="flex grid-cols-3">
         <img class="w-12 h-12" src="/public/images/Sectiontournois.png" alt="">
      <div class=""> <h1 class="text-black text-5xl px-4 font-rajdhani">Annonces</h1><h1 class="text-green-600 text-5xl font-rajdhani"> Tournois </h1>
-       <p class="text-black text-2xl font-rajdhani font-light">Retrouvez le tournoi esport League of Legend afin de départager les 
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-rajdhani font-light">Retrouvez le tournoi esport League of Legend afin de départager les 
 meilleures équipes universitaires de  Bourgogne Franche-Comté qui 
 participera à la grosse ligue.</p>
        <br>
 <br>
-       <p class="text-black text-2xl font-rajdhani font-normal">Vous retouverez ici l’agenda des match du tournoi ainsi que les rediffusion 
+       <p class="text-black flex pl-5 pt-5 pb-10 sm:text-base lg:text-xl xl:text-2xl font-rajdhani font-normal">Vous retouverez ici l’agenda des match du tournoi ainsi que les rediffusion 
 des matchs.</p></div>
 </div>
 <div class="grid grid-cols-1">
